a27c1f490ab924e49cd11b6b8bbcb6607ba4d969
maram-brg <Maram.Bourguiba@esprit.tn>
2026-05-26 17:42:16 +0100

    Added Project and Task models + migrations

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'project_id',
+    ];
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+}
